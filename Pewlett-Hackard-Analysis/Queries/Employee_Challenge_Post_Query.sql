93eb2926154ebafa089f6830e30128b15a91a9db
Marcokovich <71575748+Marcokovich@users.noreply.github.com>
2022-02-13 12:35:23 -0700

    Update Employee_Challenge_Post_Query.sql

--- Pewlett-Hackard-Analysis/Queries/Employee_Challenge_Post_Query.sql
+++ Pewlett-Hackard-Analysis/Queries/Employee_Challenge_Post_Query.sql
@@ -1,12 +1,23 @@
---DROP TABLE mentor_count;
-SELECT COUNT(me.emp_no)
-INTO mentor_count
-FROM mentorship_eligibility as me;
---DROP TABLE retiring_count;
-SELECT SUM(rt.count)
-INTO retiring_count
-FROM retiring_titles as rt;
-SELECT rc.sum,
-mc.count
-FROM mentor_count as mc,
-retiring_count as rc;
+--DROP TABLE mentorship_titles;
+SELECT COUNT(me.title),
+me.title
+INTO mentorship_titles
+FROM mentorship_eligibility as me
+GROUP BY me.title
+ORDER BY count DESC;
+ALTER TABLE mentorship_titles
+RENAME count to mentee_count;
+
+--DROP TABLE mentee_count;
+SELECT mt.title,
+mt.mentee_count,
+rt.count
+INTO mentee_count
+FROM mentorship_titles as mt
+INNER JOIN retiring_titles as rt
+ON mt.title = rt.title
+ORDER BY rt.count DESC;
+ALTER TABLE mentee_count
+RENAME count to mentor_count;
+
+SELECT * FROM public.mentee_count
